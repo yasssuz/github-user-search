2c79cf25540ffd5579b16b295a27431a758cd8b2
Karim Yassine <karimwebdevelopment@gmail.com>
2021-09-27 14:53:05 -0300

    added TopArea component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import styled from "styled-components";
+import { ThemeContextProvider } from "./contexts/ThemeContext";
+import TopArea from "./components/TopArea";
+
 export default function App() {
   return (
-    <div className='App'>
-      <h1>test</h1>
-    </div>
+    <ThemeContextProvider>
+      <Container>
+        <TopArea />
+      </Container>
+    </ThemeContextProvider>
   );
 }
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: center;
+  align-items: center;
+  justify-content: center;
+  background: ${props => props.theme.colors.background};
+  padding: 3.1rem 2.4rem;
+
+  @media (min-width: 768px) {
+    padding: 3.1rem 4rem;
+  }
+`;
