848912b0270d700f3e55d0f5db9f9dcf683af8b8
Karim Yassine <karimwebdevelopment@gmail.com>
2021-09-27 14:54:00 -0300

    added personalized theming

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, FC, useState } from "react";
+import { ThemeProvider } from "styled-components";
+
+interface ThemeContextProps {
+  changeTheme: () => void;
+  lightMode: boolean;
+}
+
+export const ThemeContext = createContext({} as ThemeContextProps);
+
+export const ThemeContextProvider: FC = ({ children }) => {
+  const [lightMode, setLightMode] = useState<boolean>(false);
+  const darkTheme: object = {
+    colors: {
+      background: "#141D2F",
+      themeBtn: "#fff",
+      card: "#1E2A47",
+      textNorm: "#fff",
+    },
+  };
+  const lightTheme: object = {
+    colors: {
+      background: "#F6F8FF",
+      themeBtn: "#4B6A9B",
+      card: "#FEFEFE",
+      textNorm: "#697C9A",
+    },
+  };
+
+  function changeTheme(): void {
+    setLightMode(prev => !prev);
+  }
+
+  return (
+    <ThemeContext.Provider value={{ changeTheme, lightMode }}>
+      <ThemeProvider theme={lightMode ? lightTheme : darkTheme}>
+        {children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
