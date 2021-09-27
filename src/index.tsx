3532ed5243f67279c15e094f20e3012b012ecbd3
Karim Yassine <karimwebdevelopment@gmail.com>
2021-09-27 14:52:28 -0300

    added global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createGlobalStyle } from "styled-components";
 import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+    overflow-x: hidden;
+  }
+
+  body, 
+  input, 
+  button, 
+  textarea, 
+  select {
+    font-family: 'Space Mono', sans-serif;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
