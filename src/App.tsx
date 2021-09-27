e45a4d890a6857e47045c2b3de6b47511413f51e
Karim Yassine <karimwebdevelopment@gmail.com>
2021-09-27 11:00:29 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>test</h1>
     </div>
   );
 }
-
-export default App;
