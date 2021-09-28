84b584a24dd24b1a9987c9f6b8c86856ff41bfae
Karim Yassine <karimwebdevelopment@gmail.com>
2021-09-27 23:43:35 -0300

    added user search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import styled from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
 import TopArea from "./components/TopArea";
+import MainArea from "./components/MainArea";
+import { useState } from "react";
+
+const queryClient = new QueryClient();
 
 export default function App() {
+  const [username, setUsername] = useState<string>("");
+
+  function changeUsername(username: string): void {
+    setUsername(username);
+  }
+
   return (
-    <ThemeContextProvider>
-      <Container>
-        <TopArea />
-      </Container>
-    </ThemeContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContextProvider>
+        <Container>
+          <TopArea changeUsername={changeUsername} />
+          {username && <MainArea username={username} />}
+        </Container>
+      </ThemeContextProvider>
+    </QueryClientProvider>
   );
 }
 
 const Container = styled.main`
   min-height: 100vh;
   display: flex;
-  flex-direction: center;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background: ${props => props.theme.colors.background};
