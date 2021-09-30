cd8d64e83dbdc4f0b044564f5e690d728c6b9aa7
Karim Yassine <karimwebdevelopment@gmail.com>
2021-09-30 14:38:19 -0300

    fixed bugs

--- src/components/MainArea.tsx
+++ src/components/MainArea.tsx
@@ -1,11 +1,13 @@
 import { useQuery } from "react-query";
 import UserData from "./UserData/index";
+import { joinedDate } from "../utils/formatters";
 
 interface MainAreaProps {
   username: string;
 }
 
 interface UserProps {
+  pfp: string;
   name: string;
   joinedAt: string;
   username: string;
@@ -26,32 +28,33 @@ function getUser(username: string): any {
     const res = await fetch(`https://api.github.com/users/${username}`);
     const data = await res.json();
 
-    return data;
+    const user: UserProps = {
+      pfp: data.avatar_url,
+      name: data.name,
+      joinedAt: joinedDate(data.created_at),
+      username: data.login,
+      bio: data.bio,
+      repos: data.public_repos,
+      followers: data.followers,
+      following: data.following,
+      links: {
+        location: data.location,
+        twitter: data.twitter_username,
+        company: data.company,
+        blog: data.blog,
+      },
+    };
+
+    return user;
   });
 }
 
 export default function MainArea({ username }: MainAreaProps) {
-  const { data, status, isFetching } = getUser(username);
+  const { data: user, status, isFetching } = getUser(username);
 
-  if (isFetching || status === "loading") return <h1>Loading...</h1>;
+  if (isFetching || status === "loading") return null;
 
-  if (data.message === "Not Found") return <h1>Not Found...</h1>;
-
-  const user: UserProps = {
-    name: data.name,
-    joinedAt: data.created_at,
-    username: data.login,
-    bio: data.bio,
-    repos: data.public_repos,
-    followers: data.followers,
-    following: data.following,
-    links: {
-      location: data.location,
-      twitter: data.twitter_username,
-      company: data.company,
-      blog: data.blog,
-    },
-  };
+  if (user.message === "Not Found") return <h1>Not Found...</h1>;
 
   return <UserData user={user} />;
 }
