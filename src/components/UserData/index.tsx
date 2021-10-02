f7bb0e493e30c13e3d4f12f115dfde05970e18ff
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-02 18:27:05 -0300

    added pfp props

--- src/components/UserData/index.tsx
+++ src/components/UserData/index.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
+import TopArea from "./TopArea";
 import LinksArea from "./LinksArea";
 import StatsArea from "./StatsArea";
 
 interface UserDataProps {
   user: {
+    pfp: string;
     name: string;
     joinedAt: string;
     username: string;
@@ -23,13 +25,22 @@ interface UserDataProps {
 export default function UserData({ user }: UserDataProps) {
   return (
     <Container>
-      <h1>Still working here</h1>
-      <StatsArea
-        repos={user.repos}
-        followers={user.followers}
-        following={user.following}
-      />
-      <LinksArea links={user.links} />
+      <Pfp src={user.pfp} alt={user.name} />
+      <SideArea>
+        <TopArea
+          username={user.username}
+          bio={user.bio}
+          name={user.name}
+          joinedAt={user.joinedAt}
+          pfp={user.pfp}
+        />
+        <StatsArea
+          repos={user.repos}
+          followers={user.followers}
+          following={user.following}
+        />
+        <LinksArea links={user.links} />
+      </SideArea>
     </Container>
   );
 }
@@ -42,8 +53,38 @@ const Container = styled.section`
   margin-top: 1.6rem;
   max-width: 73.3rem;
   box-shadow: 0px 16px 30px -10px rgba(70, 96, 187, 0.198567);
+  display: flex;
 
   @media (min-width: 768px) {
     padding: 5.2rem 4.8rem;
   }
+
+  @media (min-width: 900px) {
+    padding: 4.8rem;
+  }
+
+  a {
+    all: unset;
+    cursor: pointer;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const Pfp = styled.img`
+  height: 117px;
+  width: 117px;
+  border-radius: 50%;
+  margin-right: 3.7rem;
+  display: none;
+
+  @media (min-width: 900px) {
+    display: block;
+  }
+`;
+
+const SideArea = styled.div`
+  width: 100%;
 `;
