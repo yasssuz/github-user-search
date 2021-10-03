a7dd3e355df481a3faf23a2aebf4d54192b903c1
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-02 23:05:13 -0300

    code refactor

--- src/components/MainArea.tsx
+++ src/components/MainArea.tsx
@@ -1,56 +1,10 @@
-import { useQuery } from "react-query";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 import UserData from "./UserData/index";
-import { joinedDate } from "../utils/formatters";
 
-interface MainAreaProps {
-  username: string;
-}
-
-interface UserProps {
-  pfp: string;
-  name: string;
-  joinedAt: string;
-  username: string;
-  bio: string;
-  repos: string;
-  followers: string;
-  following: string;
-  links: {
-    location: string;
-    twitter: string;
-    blog: string;
-    company: string;
-  };
-}
-
-function getUser(username: string): any {
-  return useQuery("user", async () => {
-    const res = await fetch(`https://api.github.com/users/${username}`);
-    const data = await res.json();
-
-    const user: UserProps = {
-      pfp: data.avatar_url,
-      name: data.name,
-      joinedAt: joinedDate(data.created_at),
-      username: data.login,
-      bio: data.bio,
-      repos: data.public_repos,
-      followers: data.followers,
-      following: data.following,
-      links: {
-        location: data.location,
-        twitter: data.twitter_username,
-        company: data.company,
-        blog: data.blog,
-      },
-    };
-
-    return user;
-  });
-}
-
-export default function MainArea({ username }: MainAreaProps) {
-  const { data: user, status, isFetching } = getUser(username);
+export default function MainArea() {
+  const { getUser } = useContext(UserContext);
+  const { data: user, status, isFetching } = getUser();
 
   if (isFetching || status === "loading") return null;
 
