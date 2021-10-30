15a7f42b1db308eef579ecf02232df8efb1fdc96
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-29 23:20:51 -0300

    added new types

---
+++ src/types/index.ts
@@ -0,0 +1,28 @@
+export interface UserProps {
+  pfp: string;
+  name: string;
+  joinedAt: string;
+  username: string;
+  bio: string;
+  repos: string;
+  followers: string;
+  following: string;
+  links: {
+    location: string;
+    twitter: string;
+    blog: string;
+    company: string;
+  };
+}
+
+export interface TopAreaProps {
+  setUser: (user: UserProps) => void;
+}
+
+export interface UserDataProps {
+  user: UserProps;
+}
+
+export interface MainAreaProps {
+  user: UserProps;
+}
